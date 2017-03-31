ef08495bf9b20d9a061dd8a5f0d2413f087a8a72
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2017-03-31 13:12:23 +1000

    Add support for 3 new views color list similarity

--- src/routes.js
+++ src/routes.js
@@ -6,31 +6,20 @@ const routes = Router();
  * GET home page
  */
 routes.get('/', (req, res) => {
-  res.render('index', { title: 'Express Babel' });
+  res.render('index', { title: 'Outler' });
 });
 
-/**
- * GET /list
- *
- * This is a sample route demonstrating
- * a simple approach to error handling and testing
- * the global error handler. You most certainly want to
- * create different/better error handlers depending on
- * your use case.
- */
-routes.get('/list', (req, res, next) => {
-  const { title } = req.query;
-
-  if (title == null || title === '') {
-    // You probably want to set the response HTTP status to 400 Bad Request
-    // or 422 Unprocessable Entity instead of the default 500 of
-    // the global error handler (e.g check out https://github.com/kbariotis/throw.js).
-    // This is just for demo purposes.
-    next(new Error('The "title" parameter is required'));
-    return;
-  }
-
-  res.render('index', { title });
+routes.get('/color', (req, res) => {
+  res.render('color', { title: 'Color' });
 });
+
+routes.get('/similarity', (req, res) => {
+  res.render('similarity', { title: 'Similarity' });
+});
+
+routes.get('/list', (req, res) => {
+  res.render('list', { title: 'List' });
+});
+
 
 export default routes;
