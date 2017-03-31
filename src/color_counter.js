325ff2b9d00c7873d3e09c8362d8455f6192cad1
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2017-03-31 14:39:32 +1000

    Improved hex -> rgb conversion

--- src/color_counter.js
+++ src/color_counter.js
@@ -1,19 +1,5 @@
 var fs = require('fs');
-
-function hexToRgb(hex) {
-    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-        return r + r + g + g + b + b;
-    });
-
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-    } : null;
-}
+var hexRgb = require('hex-rgb');
 
 export default function colorCounter(filePath) {
   var cssParser = require('css');
@@ -31,7 +17,7 @@ export default function colorCounter(filePath) {
       }
 
       if(declaration['value'].includes('#')) {
-        colors.set(declaration['value'], { rgb: hexToRgb(declaration['value']),
+        colors.set(declaration['value'], { rgb: hexRgb(declaration['value']),
                                            count: (colors.get(declaration['value']) || {count: 0}).count + 1
         })
       }
