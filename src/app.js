18d75b1e0886cbeb3e1b7f5ca59b38b02746e252
Lennon Cheng <lennon9469@gmail.com>
2017-05-31 17:12:25 +1000

    add clipboard to the project and load in color

--- src/app.js
+++ src/app.js
@@ -26,6 +26,7 @@ app.use(logger('dev', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
+app.use('/js', express.static(path.join(__dirname, '../node_modules/clipboard/dist')));
 
 // Routes
 app.use('/', routes);
