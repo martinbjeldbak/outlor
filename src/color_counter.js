df006cbff591406f16557d1ab3052be5a8aebdae
Lennon Cheng <lennon9469@gmail.com>
2017-06-09 16:41:07 +1000

    convert #xxx to #XXXXXX

--- src/color_counter.js
+++ src/color_counter.js
@@ -8,16 +8,46 @@ export default function colorCounter(file) {
   ast.stylesheet.rules.forEach((rule) => {
     if (rule.declarations === undefined) return;
 
+
     rule.declarations.forEach((declaration) => {
       if (!(declaration.property === 'color')) return;
 
       let value = declaration.value;
 
+      // remove !important tag if there is one
       if (value.includes('important')) {
         value = value.replace('!important', '');
       }
 
+      // if value is a hexa color
+      if (value.includes('#')) {
+        // if length of value is 4 (short form of hexa code)
+        if (value.length == 4) {
+          value = value.replace('#', '');
+          value = value.split("").map((x) => x.repeat(2)).join('');
+          value = '#' + value;
+        }
+
+      } else {
+        // if value is not a hexa color
+        console.log("not a hexa color");
+
+        // color name
+
+        // rgb
+
+        // rgba
+
+        // hsl
+      }
+
+      // inherit
+
+      // transparent
+
       value = value.trim();
+
+      console.log(value);
 
       data.set(value, {
         rgb: hexRgb(value),
